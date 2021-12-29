952e2ca5344dd0d64b151f7a0cfc8921a9e3801d
samkcdev <samnp87@gmail.com>
2021-12-29 13:16:16 +0530

    added 11ty navigation package

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
-module.exports = function (eleventyConfig) {
-  // Output directory: _site
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  // Output directory: _site
   eleventyConfig.addWatchTarget("assets");
 
   //passthrough files
   eleventyConfig.addPassthroughCopy("assets/img");
   eleventyConfig.addPassthroughCopy("assets/fonts");
+  eleventyConfig.addPassthroughCopy(
+    "assets/pdf/SamKCherianResume-FrontendDeveloper.pdf"
+  );
   // eleventyConfig.addPassthroughCopy("assets/styles");
 
   return {
