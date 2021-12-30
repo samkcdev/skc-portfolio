974b046fa9d79f009ae1910f1958b4ea7ae1c3bc
samkcdev <samnp87@gmail.com>
2021-12-30 13:59:56 +0530

    Added collection to be added from posts

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,10 @@ module.exports = function (eleventyConfig) {
   );
   // eleventyConfig.addPassthroughCopy("assets/styles");
 
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/posts/*.md");
+  });
+
   return {
     dir: {
       input: "src",
