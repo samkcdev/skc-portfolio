a7030b60e31c7bec4866fc165723319d9c7f5e51
samkcdev <samnp87@gmail.com>
2022-02-02 17:53:27 +0530

    style changes to post styles and added filter to post collection

--- .eleventy.js
+++ .eleventy.js
@@ -14,13 +14,16 @@ module.exports = function (eleventyConfig) {
   // eleventyConfig.addPassthroughCopy("assets/styles");
 
   eleventyConfig.addCollection("posts", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("./src/posts/*.md");
+    return collectionApi
+      .getFilteredByGlob("./src/posts/*.md")
+      .filter((item) => item.data.permalink !== false);
   });
 
   eleventyConfig.addCollection("featuredPosts", function (collectionApi) {
     return collectionApi
       .getFilteredByGlob("./src/posts/*.md")
-      .filter((item) => item.data.featured);
+      .filter((item) => item.data.featured)
+      .filter((item) => item.data.permalink !== false);
   });
 
   return {
